df9d8877f03ca33857dfec07e7fef14d3219b989
ak15 <atulkanswal@gmail.com>
2023-02-22 12:48:35 +0530

    commit

--- ruby/ext/google/protobuf_c/utf8_range.h
+++ ruby/ext/google/protobuf_c/utf8_range.h
@@ -1,55 +1,21 @@
-#ifndef GOOGLE_PROTOBUF_UTF8_RANGE_H__
-#define GOOGLE_PROTOBUF_UTF8_RANGE_H__
+#ifndef THIRD_PARTY_UTF8_RANGE_UTF8_RANGE_H_
+#define THIRD_PARTY_UTF8_RANGE_UTF8_RANGE_H_
 
-#include <cstdint>
-#include <cstring>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-namespace google {
-namespace protobuf {
+#if (defined(__ARM_NEON) && defined(__aarch64__)) || defined(__SSE4_1__)
+int utf8_range2(const unsigned char* data, int len);
+#else
+int utf8_naive(const unsigned char* data, int len);
+static inline int utf8_range2(const unsigned char* data, int len) {
+  return utf8_naive(data, len);
+}
+#endif
 
-class Utf8Range {
- public:
-  Utf8Range(const char* begin, const char* end)
-    : begin_(begin), end_(end) {}
+#ifdef __cplusplus
+}  // extern "C"
+#endif
 
-  const char* begin() const { return begin_; }
-  const char* end() const { return end_; }
-
-  uint32_t Decode(const char*& ptr) const {
-    const unsigned char* p = reinterpret_cast<const unsigned char*>(ptr);
-    uint32_t c = *p++;
-    int extra_bytes = 0;
-    if (c >= 0x80) {
-      c = (c & 0x7F) << 6;
-      uint32_t d = *p++;
-      if ((d & 0xC0) != 0x80) return 0;
-      c |= (d & 0x3F);
-      extra_bytes = 1;
-    }
-    if (c >= 0x800) {
-      c = (c & 0x7FF) << 6;
-      uint32_t d = *p++;
-      if ((d & 0xC0) != 0x80) return 0;
-      c |= (d & 0x3F);
-      extra_bytes = 2;
-    }
-    if (c >= 0x10000) {
-      c = (c & 0xFFFF) << 6;
-      uint32_t d = *p++;
-      if ((d & 0xC0) != 0x80) return 0;
-      c |= (d & 0x3F);
-      extra_bytes = 3;
-    }
-    ptr += extra_bytes + 1;
-    return c;
-  }
-
- private:
-  const char* begin_;
-  const char* end_;
-};
-
-}  // namespace protobuf
-}  // namespace google
-
-#endif  // GOOGLE_PROTOBUF_UTF8_RANGE_H__
+#endif  // THIRD_PARTY_UTF8_RANGE_UTF8_RANGE_H_
