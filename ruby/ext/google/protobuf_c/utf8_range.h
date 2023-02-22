721cf0b916dffde0a8e28ae4a872b31970856523
ak15 <atulkanswal@gmail.com>
2023-02-22 12:11:29 +0530

    Utf8 Range

--- ruby/ext/google/protobuf_c/utf8_range.h
+++ ruby/ext/google/protobuf_c/utf8_range.h
@@ -1,55 +1,55 @@
-#ifndef UTF8_RANGE_H
-#define UTF8_RANGE_H
+#ifndef GOOGLE_PROTOBUF_UTF8_RANGE_H__
+#define GOOGLE_PROTOBUF_UTF8_RANGE_H__
 
+#include <cstdint>
+#include <cstring>
 
-namespace utf8 {
+namespace google {
+namespace protobuf {
 
-  // Define some constants
-  const uint32_t kMask[] = {0x7F, 0x1F, 0x0F, 0x07};
-  const uint32_t kMagic[] = {0x00, 0xC0, 0xE0, 0xF0};
+class Utf8Range {
+ public:
+  Utf8Range(const char* begin, const char* end)
+    : begin_(begin), end_(end) {}
 
-  // Define the utf8_range class
-  class utf8_range {
-  public:
-    utf8_range(const char* str) : str_(str) {}
-    ~utf8_range() {}
+  const char* begin() const { return begin_; }
+  const char* end() const { return end_; }
 
-    const char* begin() const { return str_; }
-    const char* end() const { return str_ + std::strlen(str_); }
-
-    uint32_t decode(const char*& ptr) const {
-      const unsigned char* p = reinterpret_cast<const unsigned char*>(ptr);
-      uint32_t c = *p++;
-      int extra_bytes = 0;
-      if (c >= 0x80) {
-        c = (c & kMask[1]) << 6;
-        uint32_t d = *p++;
-        if ((d & 0xC0) != 0x80) return 0;
-        c |= (d & 0x3F);
-        extra_bytes = 1;
-      }
-      if (c >= 0x800) {
-        c = (c & kMask[2]) << 6;
-        uint32_t d = *p++;
-        if ((d & 0xC0) != 0x80) return 0;
-        c |= (d & 0x3F);
-        extra_bytes = 2;
-      }
-      if (c >= 0x10000) {
-        c = (c & kMask[3]) << 6;
-        uint32_t d = *p++;
-        if ((d & 0xC0) != 0x80) return 0;
-        c |= (d & 0x3F);
-        extra_bytes = 3;
-      }
-      ptr += extra_bytes + 1;
-      return c;
+  uint32_t Decode(const char*& ptr) const {
+    const unsigned char* p = reinterpret_cast<const unsigned char*>(ptr);
+    uint32_t c = *p++;
+    int extra_bytes = 0;
+    if (c >= 0x80) {
+      c = (c & 0x7F) << 6;
+      uint32_t d = *p++;
+      if ((d & 0xC0) != 0x80) return 0;
+      c |= (d & 0x3F);
+      extra_bytes = 1;
     }
+    if (c >= 0x800) {
+      c = (c & 0x7FF) << 6;
+      uint32_t d = *p++;
+      if ((d & 0xC0) != 0x80) return 0;
+      c |= (d & 0x3F);
+      extra_bytes = 2;
+    }
+    if (c >= 0x10000) {
+      c = (c & 0xFFFF) << 6;
+      uint32_t d = *p++;
+      if ((d & 0xC0) != 0x80) return 0;
+      c |= (d & 0x3F);
+      extra_bytes = 3;
+    }
+    ptr += extra_bytes + 1;
+    return c;
+  }
 
-  private:
-    const char* str_;
-  };
+ private:
+  const char* begin_;
+  const char* end_;
+};
 
-}  // namespace utf8
+}  // namespace protobuf
+}  // namespace google
 
-#endif  // UTF8_RANGE_H
+#endif  // GOOGLE_PROTOBUF_UTF8_RANGE_H__
